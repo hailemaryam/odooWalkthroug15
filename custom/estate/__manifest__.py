20795ccecccabaf95c822e1b4aa87bc9d98e6bca
hailemaryam <hailemaryammecca@gmail.com>
2022-03-31 12:12:03 +0300

    from end of chapter 12

---
+++ custom/estate/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "estate",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
+        'views/estate_menus.xml',
+        # 'views/templates.xml',
+    ],
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
